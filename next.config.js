5726692408e6bf42f8b263933447195ceb3aa2aa
marczieee <stem.geleram@gmail.com>
2026-07-30 23:11:30 +0800

    commit

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
-   const nextConfig = {
-     output: 'export',
-     trailingSlash: true,
-     images: { unoptimized: true },
-   };
+const nextConfig = {
+  trailingSlash: true,
+  images: { unoptimized: true },
+};
 
-   module.exports = nextConfig;
+module.exports = nextConfig;
